f3918ab6962d00b6b1f32a166a1541826fa81353
Adi Primanda Ginting <adi@adipginting.com>
2026-08-03 22:19:29 +0700

    fix: row

--- app/components/sections/organizers/organizers.tsx
+++ app/components/sections/organizers/organizers.tsx
@@ -117,7 +117,9 @@ export const OrganizersSection = ({ organizers }: OrganizersSectionProps) => {
 					}
 				}
 			});
-		}
+    }
+
+    console.log("PARTNERSHIP ", partnership)
 
 		return [
 			{ name: "Lead Organizer", items: lead },
@@ -182,7 +184,8 @@ export const OrganizersSection = ({ organizers }: OrganizersSectionProps) => {
 									group.items.length === 1 &&
 										"sm:grid-cols-1 lg:grid-cols-1 max-w-xl",
 									group.items.length === 2 &&
-										"sm:grid-cols-2 lg:grid-cols-2 max-w-4xl",
+                  "sm:grid-cols-2 lg:grid-cols-2 max-w-4xl",
+                  group.items.length > 1 && "lg:[&>:nth-child(3n+1):last-child]:col-start-2"
 								)}
 							>
 								{group.items.length > 0 ? (
